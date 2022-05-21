bd4baf8ff3361c958566210107ec70f30bef9d99
kdmnkk <kdmnk.an@gmail.com>
2022-05-21 17:27:02 +0300

    create service for todos

--- src/app/components/todo/add-task-form/add-task-form.component.ts
+++ src/app/components/todo/add-task-form/add-task-form.component.ts
@@ -1,14 +1,29 @@
-import { Component, Output, EventEmitter } from '@angular/core';
-import {FormBuilder, FormControl, Validators} from "@angular/forms";
+import { Component, Output, EventEmitter, HostListener, ElementRef, Input } from '@angular/core';
+import { FormBuilder, FormControl, Validators } from "@angular/forms";
 
 @Component({
   selector: 'app-add-task-form',
   templateUrl: './add-task-form.component.html',
   styleUrls: ['./add-task-form.component.scss']
 })
-export class AddTaskFormComponent {
 
+export class AddTaskFormComponent {
+  @Input() open!: boolean;
+  @Input() button!: any;
   @Output() newItemEvent = new EventEmitter<boolean>();
+
+  @HostListener('window:click', ['$event.target'])
+  CloseFormWithoutBtn(event: any) {
+    if(this.open) {
+      if (
+        !this._eref.nativeElement.contains(event)
+        && !this.button._elementRef.nativeElement.contains(event)
+        && !event.className.includes('calendar')
+      ) {
+        this.closeForm();
+      }
+    }
+  }
 
   form = this.fb.group({
     title: new FormControl('', [Validators.required, Validators.minLength(1), Validators.maxLength(30)]),
@@ -16,11 +31,9 @@ export class AddTaskFormComponent {
     description: new FormControl('', [Validators.maxLength(200)]),
   });
 
-  constructor(private fb: FormBuilder) {
-  }
+  constructor(private fb: FormBuilder, private _eref: ElementRef) {}
 
   submit() {
-    console.log('111')
     console.log(this.form);
   }
 
